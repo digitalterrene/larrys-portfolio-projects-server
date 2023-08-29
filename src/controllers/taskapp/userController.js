ed9645d338d99dde2962144338bbd0334f68d47d
digitalterrene <110802979+digitalterrene@users.noreply.github.com>
2023-08-29 06:22:12 +0200

    added an API endpoint to fetch app users

--- src/controllers/taskapp/userController.js
+++ src/controllers/taskapp/userController.js
@@ -19,7 +19,21 @@ const signupUser = async (req, res) => {
     }
   }
 };
-
+//get users
+const fatechUsers = async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    if (users) {
+      res.status(200).json(users);
+    } else {
+      //requested user not found
+      res.status(404).json({ error: "No users found" });
+    }
+  } catch (error) {
+    //server crashed || something went wrong with the server
+    res.status(500).json({ error: error.message });
+  }
+};
 //get user
 const signinUser = async (req, res) => {
   const { username, password } = req.body;
